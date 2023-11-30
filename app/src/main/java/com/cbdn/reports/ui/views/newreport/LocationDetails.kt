8ce87f0caa6f1451b6db7240a9769ec113eb4c3a
Richard Doherty <dohertri@oregonstate.edu>
2023-11-30 15:31:55 -0800

    Removed LocationTextField Test File and added comment for unknown flag

--- app/src/main/java/com/cbdn/reports/ui/views/newreport/LocationDetails.kt
+++ app/src/main/java/com/cbdn/reports/ui/views/newreport/LocationDetails.kt
@@ -1,9 +1,7 @@
 package com.cbdn.reports.ui.views.newreport
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
