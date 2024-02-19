626acd87cb46f441d6eecbbdb123b0d2223b5bde
Christopher Murphy <murphyc3@oregonstate.edu>
2024-02-19 17:20:54 -0500

    Added prepopulation of location details to form

--- app/src/main/java/com/cbdn/reports/ui/views/newreport/LocationDetails.kt
+++ app/src/main/java/com/cbdn/reports/ui/views/newreport/LocationDetails.kt
@@ -26,24 +26,73 @@ import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import com.google.android.gms.location.FusedLocationProviderClient
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
+
 
 @Composable
 fun LocationDetails(
     viewModel: AppViewModel,
     modifier: Modifier
 ) {
+    // Declaring our variables
     val reportState by viewModel.reportState.collectAsStateWithLifecycle()
     var pinLocation by remember { mutableStateOf(LatLng(0.0,0.0))}
     val coroutineScope = rememberCoroutineScope()
     val santoDomingo = LatLng(18.46, -69.94)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(santoDomingo, 10f)
+    val context = LocalContext.current as Activity
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    var lastKnownLocation: Location? = null
+    var cameraPositionState = rememberCameraPositionState {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            val locationResult = fusedLocationProviderClient.lastLocation
+            locationResult.addOnCompleteListener(context) { task ->
+                if (task.isSuccessful) {
+                    // Get the location
+                    lastKnownLocation = task.result
+                    if (lastKnownLocation != null) {
+                        // Positions the map
+                        position = CameraPosition.fromLatLngZoom(LatLng(
+                            lastKnownLocation!!.latitude,
+                            lastKnownLocation!!.longitude
+                        ),
+                            10f
+                        )
+                        // Place the pin
+                        pinLocation = LatLng(
+                            lastKnownLocation!!.latitude,
+                            lastKnownLocation!!.longitude
+                        )
+                        // Get the address using the lat and long
+                        coroutineScope.launch(Dispatchers.IO) {
+                            viewModel.getAddress(LatLng(
+                                lastKnownLocation!!.latitude,
+                                lastKnownLocation!!.longitude
+                            ), BuildConfig.MAPS_API_KEY)
+                        }
+                    }
+                        }
+                    }
+        } else {
+            position = CameraPosition.fromLatLngZoom(santoDomingo, 10f)
+        }
     }
+
+    // Create the layout of the page
     Column(
         modifier = modifier
             .background(color = MaterialTheme.colorScheme.background),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        // Stuff to go in the column
         FormHeader(textResource = R.string.submit_location_header)
 
         // LOCATION
